8d0d02f052d872534701768e6ccacd4f22daa3cc
oprah <oprah.chemirmir@student.moringaschool.com>
2022-12-22 14:41:21 +0300

    done with project

--- index.js
+++ index.js
@@ -58,16 +58,6 @@ function showMovies(movies) {
     });
 }
 
-function getClassByRate(vote) {
-    if (vote >= 8) {
-        return "green";
-    } else if (vote >= 5) {
-        return "orange";
-    } else {
-        return "red";
-    }
-}
-
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 
