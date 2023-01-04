24fbc5a36dc1dc30e4528cf142fffe9950cd3b41
oprah <oprah.chemirmir@student.moringaschool.com>
2023-01-04 20:47:07 +0300

    done with project

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 const APIURL = "https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=2014-09-15&primary_release_date.lte=2014-10-22&api_key=04c35731a5ee918f014970082a0088b1&page=1";
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
@@ -8,12 +6,14 @@ const main = document.getElementById("main");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
 const search_res = document.getElementById("search_res");
+const total_records = document.getElementById("total_records");
 
 // initially get fav movies
 fetchMovies(APIURL);
 
-async function getMovies(url) {
+async function fetchMovies(url) {
     const resp = await fetch(url);
+    const respData = await resp.json();
 
     if (respData.results.length == 0) {
         search_res.innerHTML = "No Results Founds";
@@ -57,6 +57,16 @@ function showMovies(movies) {
     });
 }
 
+function getClassByRate(vote) {
+    if (vote >= 8) {
+        return "green";
+    } else if (vote >= 5) {
+        return "orange";
+    } else {
+        return "red";
+    }
+}
+
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 
@@ -64,7 +74,7 @@ form.addEventListener("submit", (e) => {
 
     if (searchTerm) {
         getMovies(SEARCHAPI + searchTerm);
-
+        search.value = searchTerm;
         search_res.innerHTML = "Search Result for "+searchTerm;
     } else {
         getMovies(APIURL);
